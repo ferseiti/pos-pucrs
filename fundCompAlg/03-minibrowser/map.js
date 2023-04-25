c0d44100042859aa12c81e391789c4015ad9db7b
Fernando Seiti Furusato <ferseiti@gmail.com>
2023-04-25 15:03:30 -0300

    Final da aula 02 parte 04

---
+++ fundCompAlg/03-minibrowser/map.js
@@ -0,0 +1,13 @@
+var novoMapa = new Map();
+
+class HistoryMap{
+
+}
+// 11/11/2011 às 11 da noite
+novoMapa.set('11/11/2011 - 23:30 - URL', { title: 'Titulo do site', url: 'url sozinha'});
+novoMapa.set('11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/2011 - 23:30 - URL', {});
+novoMapa.set('11/11/2011 - 23:30 - URL', {});
+
+var resultado = novoMapa.get('11/11/2011 - 23:30 - URL');
+console.log(resultado);
